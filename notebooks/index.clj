3120e6a5794493c52d4ebe8fb2ca94a6cf3391ad
daslu <daniel.slutsky@gmail.com>
2024-12-13 01:00:59 +0200

    initial tutorial

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -1,2 +1,20 @@
-(ns index)
+(ns index
+  (:require [tablecloth.api :as tc]
+            [scicloj.tablemath.v1.design-matrix :as dm]
+            [scicloj.tablemath.v1.api :as tm]))
+
+
+(-> {:x (range 9)}
+    tc/dataset
+    (tc/map-columns :y [:x] (fn [x]
+                              (+ -9
+                                 (* 4 x)
+                                 (- 2 (* x x))
+                                 (+ (* x x))
+                                 (rand))))
+    (dm/create-design-matrix [:y]
+                             [[:x '(identity x)]
+                              [:x2 '(* x x)]
+                              [:x3 '(* x x x)]])
+    (tm/lm {}))
 

ee7b6e67013282d5e8f1740dee39571e891f3e44
daslu <daniel.slutsky@gmail.com>
2024-12-13 01:00:55 +0200

    lm function

--- src/scicloj/tablemath/v1/api.clj
+++ src/scicloj/tablemath/v1/api.clj
@@ -1,5 +1,26 @@
-(ns api
+(ns scicloj.tablemath.v1.api
   (:require [tablecloth.api :as tc]
             [fastmath.vector :as vec]
-            [fastmath.ml.regression :as reg]))
+            [fastmath.ml.regression :as reg]
+            [tech.v3.dataset.modelling :as ds-mod]))
 
+(defn lm
+  "Linear model"
+  ([dataset]
+   (lm dataset nil))
+  ([dataset options]
+   (let [inference-column-name (-> dataset
+                                   ds-mod/inference-target-column-names
+                                   first)
+         ds-without-target (-> dataset
+                               (tc/drop-columns [inference-column-name]))]
+     (reg/lm
+      ;; ys
+      (get dataset inference-column-name)
+      ;; xss
+      (tc/rows ds-without-target)
+      ;; options
+      (merge {:names (-> ds-without-target
+                         tc/column-names
+                         vec)}
+             options)))))

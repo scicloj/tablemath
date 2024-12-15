797b4f407675ec5311bb86d82596c46d67f0b995
daslu <daniel.slutsky@gmail.com>
2024-12-15 15:41:50 +0200

    index page update

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -1,4 +1,12 @@
+;; Math and stats modelling with table ergonomics
+
 ;; # Preface
+
+;; This project is an initial attempt to create a Clojure library for math and statistics which is friendly to [tech.ml.dataset](https://github.com/techascent/tech.ml.dataset) and [Tablecloth](https://scicloj.github.io/tablecloth) datasets and uses the functionality of [Fastmath](https://github.com/generateme/fastmath). It is also intended to compose well with [Tableplot](https://scicloj.github.io/tableplot/) layered plotting. It is highly inspired by [R](https://www.r-project.org/) and its package.
+
+;; In a way, it is intended to be a user-friendly compatiblity layer across these libraries.
+
+;; Possibly, after the details clarify, it will be merged into one of the other Scicloj libraries.
 
 ;; Tablemath is a Clojure library for math and statistical modeling
 ;; with table ergonomics, inspired by R.
